11e50473633d2c6a3d0970f76a23325170c8ae1b
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-13 17:52:58 -0500

    feat: Add listener for changes

--- index.js
+++ index.js
@@ -7,14 +7,26 @@ function createStore () {
 
     //in charge of holding the whole state
     let state
+    let listeners = []
 
     // in change of getting the whole state
     const getState = () => state
 
+    //takes listeners array, and pushes the funciton that was called
+    //for the sake of the logs
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        //return a function filtering out the original listener if user wants to later unsubscribe
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
     //whenever createStore is envoked, they will get an object 
     //in order to access the internal state of the store
     return {
-        getState
+        getState,
+        subscribe,
     }
 
 }
