c2162041f638dd5a12371cf5805fb6e9e8ff12ff
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-13 22:41:03 -0500

    WIP: Add dispatch to actually update state
    also some light reorginizing, have createStore be passed a reducer so
    that function todos does not have to be in the same file

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// edit state by adding action
-// this is a reducer funciton
-// reduces request into a brand new state (must be a pure function)
-function todos(state = [], action) {
-    return action.type === 'ADD_TODO'
-        ? state.concat([action.todo])
-        : state
-}
-
-
-function createStore () {
+// Library code
+function createStore (reducer) {
     // in charge of the store, which is composed of...4 things
     // 1. the state
     // 2. API / way to get the state
@@ -32,11 +23,28 @@ function createStore () {
         }
     }
 
+    // responsible for updating the state in the actual store
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
     //whenever createStore is envoked, they will get an object 
     //in order to access the internal state of the store
     return {
         getState,
         subscribe,
+        dispatch,
     }
 
+}
+
+// App code
+// edit state by adding action
+// this is a reducer funciton
+// reduces request into a brand new state (must be a pure function)
+function todos(state = [], action) {
+    return action.type === 'ADD_TODO'
+        ? state.concat([action.todo])
+        : state
 }
