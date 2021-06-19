2ef832a4b94373798f8170d29ef5fc5d293b6f39
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-18 20:58:04 -0500

    fix: change listeners from strings to variables to improve future debugging

--- index.js
+++ index.js
@@ -40,16 +40,25 @@ function createStore (reducer) {
 }
 
 // App code
+
+// add variables for listeneres, this avoids posibility of typos
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+
 // edit state by adding action
 // this is a reducer funciton
 // reduces request into a brand new state (must be a pure function)
 function todos(state = [], action) {
     switch(action.type){
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo])
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
             return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map((todo) => todo.id === action.id
                 ? Object.assign({}, todo, {complete: !todo.complete} )
                 : todo)
@@ -61,9 +70,9 @@ function todos(state = [], action) {
 //goals reducer
 function goals (state = [], action) {
     switch(action.type) {
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal])
-        case 'REMOVE_GOAL':
+        case REMOVE_GOAL:
             return state.filter((goal) => goal.id !== action.id)
         default:
             return state
